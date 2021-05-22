a779e304275dd939ddc440958b65ea6c4284545a
BarbaraMoran <barbaramoranrr@gmail.com>
2021-05-22 13:58:17 +0200

    add Search Button

--- src/js/4-search.js
+++ src/js/4-search.js
@@ -1,17 +1,26 @@
 "use strict";
 
-const formEl = document.querySelector(".js-form");
+//const formEl = document.querySelector(".js-form");
 const inputEl = document.querySelector(".js-input");
-const showSearch = inputEl.value;
 const btnEl = document.querySelector(".js-button");
 
-function submitInput(event) {
+//Función Prevent Default para que la info no se envíe al navegador.
+function submitPrevent(event) {
   event.preventDefault();
 }
 
-function handleButton() {
-  console.log(showSearch);
+//Función para mostrar la búsqueda del usuario.
+function getUserSearch() {
+  let userSearch = inputEl.value;
+  //localStorage.setItem("userSearch", userSearch);
+  //const keptData = localStorage.getItem("userSearch");
+  console.log(userSearch);
+}
+
+//Función manejadora
+function handleButton(event) {
+  submitPrevent(event);
+  getUserSearch();
 }
 
 btnEl.addEventListener("click", handleButton);
-btnEl.addEventListener("click", submitInput);
